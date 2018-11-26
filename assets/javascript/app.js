8ec43b73ee6333c63897bb13b1a2baa73549f03e
JD <jdfili14@gmail.com>
2018-11-25 18:41:42 -0800

    ahaha

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var incorrect = 0;
     var unanswered = 0;
     var currentSet = 0;
-
+    var totQuestions = 7;
 
     var questions = [{
         "question": "Who was the Defensive Player of the year for the 2017-2018 season?",
@@ -18,14 +18,42 @@ $(document).ready(function () {
         "option2": "Shaq",
         "option3": "Klay Thompson",
         "option4": "Ray Allen",
-        "answer": "3"
+        "answer": "Klay Thompson"
     }, {
         "question": "Who has the most career assists in NBA history?",
         "option1": "Jason Kidd",
         "option2": "John Stockton",
         "option3": "Steve Nash",
         "option4": "Magic Johnson",
-        "answer": "2"
+        "answer": "John Stockton"
+    }, {
+        "question": "Who of these players has NOT won an MVP?",
+        "option1": "Derrick Rose",
+        "option2": "Scottie Pippen",
+        "option3": "Tim Duncan",
+        "option4": "Dirk Nowitski",
+        "answer": "Scottie Pippen"
+    }, {
+        "question": "Who is the shortest player to ever play in the NBA?",
+        "option1": "Earl Boykins",
+        "option2": "Chris Paul",
+        "option3": "Mugsy Bogues",
+        "option4": "Manute Bol",
+        "answer": "Mugsy Bogues"
+    }, {
+        "question": "Who is the winngest franchise is NBA history WITHOUT a championship?",
+        "option1": "Phoenix Suns",
+        "option2": "Oklahoma City Thunder",
+        "option3": "Charlotte Hornets",
+        "option4": "Utah Jazz",
+        "answer": "Utah Jazz"
+    }, {
+        "question": "What University did all-star player Damian Lillard play for?",
+        "option1": "Duke",
+        "option2": "Weber State",
+        "option3": "Stevens Hennager",
+        "option4": "UCLA",
+        "answer": "Weber State"
     }
     ]
 
@@ -33,28 +61,53 @@ $(document).ready(function () {
 
     $(".btn").on("click", function () {
         $(".btn").hide();
-        nextQuestion();
+        reset();
     })
 
-
-
-    function nextQuestion() {
+    function reset() {
         $(".question").text(questions[currentSet].question);
         $(".option1").text(questions[currentSet].option1);
         $(".option2").text(questions[currentSet].option2);
         $(".option3").text(questions[currentSet].option3);
         $(".option4").text(questions[currentSet].option4);
-
-        $(".guess").on("click", function () {
-            if ($(this).text() == questions[currentSet].answer) {
-                correct++
-                currentSet++
-            }
-            else if ($(this).text() != questions[currentSet].answer) {
-                incorrect++;
-                currentSet++
-            }
-            nextQuestion();
-        })
     }
+    function results() {
+        $(".game").hide();
+        $(".correct").text("Correct: " + correct);
+        $(".incorrct").text("Incorrect: " + incorrect)
+        $(".unanswered").text("Unanswered: " + unanswered)
+    }
+
+    $(".guess").on("click", function () {
+        if (totQuestions == currentSet) {
+            results()
+            return;
+        }
+        if ($(this).text() == questions[currentSet].answer) {
+            console.log(($(this).text()))
+            correct++
+            currentSet++
+            if (totQuestions == currentSet) {
+                results()
+                return;
+            }
+            else {
+                reset();
+                return;
+            }
+        }
+        else if ($(this).text() != questions[currentSet].answer) {
+            incorrect++;
+            currentSet++;
+            if (totQuestions == currentSet) {
+                results()
+                return;
+            }
+            else {
+                reset();
+                return;
+            }
+        }
+    })
+
 })
